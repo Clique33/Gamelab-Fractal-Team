9c2683bf56ce7fff4154d96b8f702db2d80b7fb1
Ralhador <Victor.vp34@gmail.com>
2025-10-15 21:07:50 -0300

    Acrescido_do_menu_de_opcoes

--- ciclo_infinito/scripts/main_menu.gd
+++ ciclo_infinito/scripts/main_menu.gd
@@ -4,6 +4,17 @@ func _on_jogar_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene/Teste_Mapa.tscn")
 	pass
 
+func _on_opcoes_button_pressed()-> void:
+	get_tree().change_scene_to_file("res://scene/MainMenu.tscn")
+	pass
+	
 func _on_creditos_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scene/Creditos.tscn")
+	get_tree().change_scene_to_file("res://scene/creditos.tscn")
+	pass
+
+func _on_sair_button_pressed() -> void:
+	get_tree().quit()
+	pass
+
+func _on_opções_button_pressed() -> void:
 	pass
