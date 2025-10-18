26c5aad21872514e7c01fb27df9ce6647c6f49cc
Ralhador <Victor.vp34@gmail.com>
2025-10-18 20:16:23 -0300

    menu_opcoes_e_pause
    crieeeeeei

--- ciclo_infinito/scripts/menu_de_opcoes.gd
+++ ciclo_infinito/scripts/menu_de_opcoes.gd
@@ -1,29 +1,33 @@
 extends Control
 
-@onready var volume_slider: HSlider = $MarginContainer/HBoxContainerGeral/volume_slider
-@onready var volume_label: Label = $MarginContainer/HBoxContainerGeral/Volume
+@onready var volume_slider: HSlider = $"MarginContainer/HBoxContainerGeral/volume_slider"
+@onready var volume_label: Label = $"MarginContainer/HBoxContainerGeral/Volume"
 
 func _ready() -> void:
-	# Define limites do slider em decibéis
-	volume_slider.min_value = -80.0
-	volume_slider.max_value = 0.0
-	volume_slider.step = 0.1
+	var master_idx = AudioServer.get_bus_index("Master")
 
-	# Define valor inicial com base no bus Master (bus 0)
-	var current_db = AudioServer.get_bus_volume_db(0)
+	volume_slider.min_value = -30.0
+	volume_slider.max_value = 0.0
+	volume_slider.step = 1
+
+	var current_db = AudioServer.get_bus_volume_db(master_idx)
 	volume_slider.value = current_db
 	_update_label(current_db)
 
-	# Conecta o sinal do slider
-	volume_slider.value_changed.connect(_on_volume_changed)
+	volume_slider.value_changed.connect(
+		func(value): _on_volume_changed(value, master_idx)
+	)
 
-func _on_volume_changed(value: float) -> void:
-	# Ajusta o volume do bus Master
-	AudioServer.set_bus_volume_db(0, value)
+func _on_volume_changed(value: float, bus_idx: int) -> void:
+	AudioServer.set_bus_volume_db(bus_idx, value)
 	_update_label(value)
 
 func _update_label(db_value: float) -> void:
-	# Converte dB para porcentagem aproximada
 	var amp = pow(10.0, db_value / 20.0)
 	var percent = int(round(amp * 100.0))
 	volume_label.text = "Volume: %d%%" % clamp(percent, 0, 100)
+
+
+func _on_voltar_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene/MainMenu.tscn")
+	pass
